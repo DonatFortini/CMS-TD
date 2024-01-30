f27e261bdb37caeb8788a61b06267eb2f4ed6d63
DonatFortini <donatfortini2a@gmail.com>
2024-01-30 20:25:57 +0100

    x

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,20 +1,20 @@
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll', (e) => {
+    e.preventDefault();
+    const scrollDirection = e.deltaY > 0 ? 'up' : 'down';
     const sections = document.querySelectorAll('[id^="section-"]');
-    const currentSection = Array.from(sections).find((section) => {
-        const rect = section.getBoundingClientRect();
-        return rect.top <= 0 && rect.bottom > 0;
-    });
-
-    if (currentSection) {
-        const idNum = Number(currentSection.id.split('-')[1]);
-        console.log(idNum);
-        const nextSection = (idNum < sections.length - 1) ? document.querySelector(`#section-${idNum + 1}`) : null;
-        const previousSection = (idNum !== 0) ? document.querySelector(`#section-${idNum - 1}`) : null;
-        // console.log(previousSection, currentSection, nextSection);
-        if (window.scrollY < currentSection.offsetTop && previousSection) {
-            previousSection.scrollIntoView({ behavior: 'smooth' });
-        } else if (window.scrollY > currentSection.offsetTop && nextSection) {
-            nextSection.scrollIntoView({ behavior: 'smooth' });
-        }
+    const currentSection = Array.from(sections).find((section) => section.classList.contains('active'));
+    console.log(currentSection);
+    const idNum = Number(currentSection.id.split(' ')[0].split('-')[1]);
+    let nextSection = null;
+    if (scrollDirection === "up" && idNum !== 0) {
+        nextSection = document.querySelector(`#section-${idNum - 1}`);
+    } else if (scrollDirection === "down" && idNum !== sections.length - 1) {
+        nextSection = document.querySelector(`#section-${idNum + 1}`);
+    }
+    if (nextSection) {
+        console.log(currentSection, nextSection);
+        nextSection.classList.add('active');
+        currentSection.classList.remove('active');
+        nextSection.scrollIntoView({ behavior: 'smooth' });
     }
 });
